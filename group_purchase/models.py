fdd5a009b0bcc3fd7ce4e186c5b207c04134a7bd
MyeongALyeong <2416011@ewha.ac.kr>
2026-03-31 13:35:05 +0900

    1:N & M:N 초기 구현 완료

--- group_purchase/models.py
+++ group_purchase/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
+class User(models.Model): #기본적인 User 모델 클래스만 생성
+  user_name = models.CharField(max_length=20)
+  password = models.CharField(max_length=128)
+  email = models.EmailField()
+
+  def __str__(self):
+    return self.user_name
+
 class Post(models.Model):
+  user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE) #작성자 필드 (1:N)
   title = models.CharField(max_length=50)
   created_at = models.DateTimeField(auto_now_add=True)
   content = models.TextField(max_length=500)
@@ -11,6 +20,21 @@ class Post(models.Model):
   max_participants = models.PositiveIntegerField()  # 인원
   location = models.CharField(max_length=100)    # 장소
 
+  #참여자 필드
+  participants = models.ManyToManyField(User, related_name='joined_posts', blank=True)
 
   def __str__(self):
     return self.title
+  
+  def summary(self):
+    return self.content[:100] #게시글 앞 100글자만 보여주는 함수
+  
+  def participant_count(self) :  #현재 참여 인원 수
+    return self.participants.count()
+  
+  def is_full(self): # 모집 마감 여부
+    return self.participants.count() >= self.max_participants
+
+
+
+
